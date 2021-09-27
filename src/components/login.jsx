5e2a24e195c01da2cfad941957635f552147ff87
unknown <pol.jefferson@hotmail.com>
2021-09-27 17:32:52 -0600

    Creando home y login component

---
+++ src/components/login.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class Login extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Formulario lógin</h1>
+                <h2>Aquí creamos el formualario para el login</h2>
+            </div>
+        )
+    }
+}
