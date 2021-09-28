5c22f95f572e18abed949ad680451cbc2574a3fd
unknown <pol.jefferson@hotmail.com>
2021-09-28 11:26:18 -0600

    Ingrando redux thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { createBrowserHistory } from 'history';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
+import thunk from 'redux-thunk';
 import createRootReducer from './rootReducer';
 
 export const history = createBrowserHistory();
@@ -9,7 +10,7 @@ export default function configureStore( preoladedState){
     const middleware = routerMiddleware(history);
     const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
     const enhacer = composeEnhancers(
-        applyMiddleware( middleware)
+        applyMiddleware(middleware,thunk)
     );
 
     const store= createStore(
@@ -19,5 +20,3 @@ export default function configureStore( preoladedState){
     )
     return store;
 }
-
-
